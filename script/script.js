e07542e2ad725157f4e22758a03ac26765f1e6ee
pmadhav82 <pmadhav279@gmail.com>
2022-07-30 22:43:19 +1000

    api fetched successfully

--- script/script.js
+++ script/script.js
@@ -14,4 +14,62 @@ menuLink.classList="nav-menu";
 })
 
 
+const URL_STRING = "https://pandey-blog.herokuapp.com/api/madhavblogs"
+const getData = async ()=>{
+  let postCard = document.querySelector(".postCard");
+  let html = "";
 
+
+  try{
+let res =  await fetch(URL_STRING);
+if(res){
+  let loader = document.querySelector(".loader");
+  loader.style.display = "none";
+}
+if(res.status>299){
+  postCard.innerHTML = `<b>Something went wrong, couldn't fetch data </b>`;
+  return
+}else{
+  let data = await res.json();
+  data.forEach((d)=>{
+    html += `
+    <div class="post_card">
+<p>
+
+   <b> ${d.creator} </b>
+   </p>
+<div class="post_date">
+   
+   <p>
+
+   ${d.createdAt}
+   </p>
+</div>
+<div class="post_title">
+
+ <h4>
+
+   ${d.title}
+ </h4>
+ </div>
+
+ <form method="post"  target = "_none" action="https://pandey-blog.herokuapp.com/post/${d._id}">
+<button class="btn" type="submit">Read More</button>
+</form>
+</div>
+    `;
+    
+  })
+}
+
+
+postCard.innerHTML = html;
+
+}catch(err){
+  console.log(err)
+}
+
+}
+
+
+getData()
